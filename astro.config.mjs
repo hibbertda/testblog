3770e4475ecf9aee3a95ddf88c11687aa567abc1
DanTheExpert <daniel@nottheexpert.us>
2024-05-21 12:52:25 +0000

    updated site  url

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://www.notanexpert.us',
   integrations: [mdx(), sitemap(), tailwind()],
   // markdown: {
   //   remarkPlugins: [
